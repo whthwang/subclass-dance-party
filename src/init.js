9af7553a19f846008b0e30937a19e966b0e584c7
whthwang <whthwang@gmail.com>
2019-04-15 17:29:55 -0700

    Finished refactoring

--- src/init.js
+++ src/init.js
@@ -15,14 +15,13 @@ $(document).ready(function() {
      * A new object of the given type will be created and added
      * to the stage.
      */
-    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name'); //'BlinkyDancer'
 
     // get the maker function for the kind of dancer we're supposed to make
-    var dancerMakerFunction = window[dancerMakerFunctionName];
-
+    var dancerMakerFunction = window[dancerMakerFunctionName]; //window['BlinkyDancer']
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction( //added keyword new to refactor to pseudoclassical style
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
